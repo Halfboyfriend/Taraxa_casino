9d3e954738e265f08d6b1d3f8d49115ea4ddbffa
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2024-01-26 19:44:19 +0100

    taraxa mainnet

--- src/utils/supportedChain.js
+++ src/utils/supportedChain.js
@@ -1,26 +1,11 @@
-
 export const supportedChain = {
-    id: 841,  // 97 is the BSC Testnet chain ID
-    idString: "0x349",  // Hex representation of the chain ID
-    name: "Taraxa Mainnet",
-    rpcUrl: "https://rpc.mainnet.taraxa.io",  // BSC Testnet RPC endpoint
-    explorerUrl: "https://explorer.mainnet.taraxa.io",  // BSC Testnet Explorer URL
-    nativeCurrency: {
-      symbol: "TARA",  // BNB is the native currency on BSC
-      decimals: 18,
-    },
-  };
-
-// export const supportedChain = {
-//   id: 56,  // 56 is the BSC Mainnet chain ID
-//   idString: "0x38",  // Hex representation of the chain ID
-//   name: "BSC Mainnet",
-//   rpcUrl: "https://bsc-dataseed.binance.org/",  // BSC Mainnet RPC endpoint
-//   explorerUrl: "https://bscscan.com/",  // BSC Mainnet Explorer URL
-//   nativeCurrency: {
-//     symbol: "BNB",  // BNB is the native currency on BSC
-//     decimals: 18,
-//   },
-// };
-
-  
+  id: 841,
+  idString: "0x349",
+  name: "Taraxa Mainnet",
+  rpcUrl: "https://rpc.mainnet.taraxa.io",
+  explorerUrl: "https://explorer.mainnet.taraxa.io",
+  nativeCurrency: {
+    symbol: "TARA",
+    decimals: 18,
+  },
+};
